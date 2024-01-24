998a2a21bf288c517ba471cf626e2235e375b642
Kenzo <kenzo@Kenzos-MBP-45.home>
2024-01-23 21:22:10 -0800

    added css hover behavior on ThemeTiles

--- src/components/ThemeTiles.tsx
+++ src/components/ThemeTiles.tsx
@@ -12,18 +12,20 @@ const ThemeTiles = ({ title, image, id }: ThemeTilesProps) => {
   return (
     <div key={title} className="item">
       <div className="theme-image">
-        <div className="flex h-full">
+        <div className={`rounded flex h-full ${image ? "" : "gradient-bg"}`}>
           <Link className="tile-scenario" href={`/scenario/${id}`}>
             <h2>{title}</h2>
           </Link>
         </div>
-        <Image
-          src={image}
-          alt={title}
-          fill
-          className="rounded"
-          sizes="(max-width:768px) 100vw, 700px"
-        />
+        {image && (
+          <Image
+            src={image}
+            alt={title}
+            fill
+            className="rounded"
+            sizes="(max-width:768px) 100vw, 700px"
+          />
+        )}
       </div>
     </div>
   );
