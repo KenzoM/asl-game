5f80f9e3fca2838450a381904beb2518b9bb8d17
Kenzo <kenzo@Kenzos-MacBook-Pro.local>
2024-02-10 23:28:40 -0800

    Add Suspense and Loading component to CanvasThree

--- src/components/CanvasThree.tsx
+++ src/components/CanvasThree.tsx
@@ -1,29 +1,33 @@
 "use client";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { AnimationProvider } from "@/app/context/AnimationContext";
 import PlayButton from "./PlayButton";
+import Loading from "@/components/Loading";
+
 const CanvasThree = ({ children }: { children: ReactNode }) => {
   return (
     <AnimationProvider>
-      <Canvas
-        style={{
-          width: "100%",
-          height: "50vh",
-          border: "1px dashed red",
-        }}
-        camera={{
-          near: 0.1,
-          far: 10000,
-          position: [0, 0.5, 5],
-        }}
-      >
-        {/* <OrbitControls /> */}
-        <ambientLight intensity={0.6} />
-        <directionalLight position={[1, 1, 1]} intensity={2} />
-        {children}
-      </Canvas>
-      <PlayButton />
+      <Suspense fallback={<Loading />}>
+        <Canvas
+          style={{
+            width: "100%",
+            height: "50vh",
+            // border: "1px dashed red",
+          }}
+          camera={{
+            near: 0.1,
+            far: 10000,
+            position: [0, 0.5, 5],
+          }}
+        >
+          {/* <OrbitControls /> */}
+          <ambientLight intensity={0.6} />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          {children}
+        </Canvas>
+        <PlayButton />
+      </Suspense>
     </AnimationProvider>
   );
 };
